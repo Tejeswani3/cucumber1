b5fbf4eae842ec145d2707d06f2275b1478b516d
GADDAM TEJESWANI <tejagaddam2000@gmail.com>
2022-09-17 13:44:02 +0530

    first commit

---
+++ src/main/java/cucum1/cucum1demo/App.java
@@ -0,0 +1,13 @@
+package cucum1.cucum1demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
